6d5f726c045477e7a72e780fc7b1a1ce45e33597
CompuScien <71332105+CompuScien@users.noreply.github.com>
2020-10-16 14:45:55 +0330

    Cart Pole with Deep Q-Learning

---
+++ DQN-CartPolev0.py
@@ -0,0 +1,139 @@
+#---------------In the name of GOD-----------#
+
+
+# THIS IS CODE FOR CARTPOLE GAME FROM OPEN-AI(gym)
+
+#dependencies
+import numpy as np
+import random
+import gym
+import os
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+
+
+#set parameters
+
+env = gym.make('CartPole-v0')
+
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+batch_size = 32
+EPISODES = 1000
+
+output_dir = 'model_output/cartpole'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+
+class DQAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.learning_rate = 0.001
+        # self.model = _build_model()
+        self.model = self._build_model()
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+
+        model.compile(loss='mse', optimizer = Adam(lr=self.learning_rate))
+
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma*(np.amax(self.model.predict(next_state)[0])))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, nb_epoch=1, verbose=0)
+
+        if self.epsilon >= self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+agent = DQAgent(state_size, action_size)
+
+#Interact with environment
+if __name__ == "__main__":
+    env = gym.make('CartPole-v0')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    # agent = DQNAgent(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            # env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+            if e % 50 == 0:
+               agent.save(output_dir + "weights_" + '{:04d}'.format(e) + ".hdf5")
+
+
+
+#Thanks to Dr Jon Krohn lecture
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
